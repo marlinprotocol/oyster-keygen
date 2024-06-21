ecf336630bc5504cb30be632137fd5200c2d35c4
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-06-21 11:49:36 +0530

    fix build scripts

--- build-amd64.sh
+++ build-amd64.sh
@@ -4,8 +4,17 @@
 
 set -e
 
-cd /code
-
 rustup target add x86_64-unknown-linux-musl
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y --no-install-recommends wget make && rm -rf /var/lib/apt/lists/*
+
+wget -O - https://musl.cc/x86_64-linux-musl-native.tgz | tar -xz
+
+export TARGET_CC=/x86_64-linux-musl-native/bin/x86_64-linux-musl-gcc
+export CC_x86_64_unknown_linux_musl=$TARGET_CC
+export RUSTC_LINKER=$TARGET_CC
+
+cd /code
 
 cargo build --release --target x86_64-unknown-linux-musl
--- build-arm64.sh
+++ build-arm64.sh
@@ -4,8 +4,17 @@
 
 set -e
 
-cd /code
-
 rustup target add aarch64-unknown-linux-musl
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y --no-install-recommends wget make && rm -rf /var/lib/apt/lists/*
+
+wget -O - https://musl.cc/aarch64-linux-musl-native.tgz | tar -xz
+
+export TARGET_CC=/aarch64-linux-musl-native/bin/aarch64-linux-musl-gcc
+export CC_aarch64_unknown_linux_musl=$TARGET_CC
+export RUSTC_LINKER=$TARGET_CC
+
+cd /code
 
 cargo build --release --target aarch64-unknown-linux-musl
